77da1c6e6aa03714dedf1e5530b94113ccf295ed
Derek <112670195+dsmdex@users.noreply.github.com>
2025-06-19 14:46:43 -0700

    commented files

--- script.py
+++ script.py
@@ -1,4 +1,54 @@
 import utils
 import sorts
 
-bookshelf = utils.load_books('books_small.csv')
+# Load small dataset of books
+books_small = utils.load_books('books_small.csv')
+
+# Sort by title (ascending)
+def compare_by_title_asc(book_a, book_b):
+  return book_a["title_lower"] > book_b["title_lower"]
+
+sorted_by_title = sorts.bubble_sort(books_small[:], compare_by_title_asc)
+
+# Sort by author (ascending)
+def compare_by_author_asc(book_a, book_b):
+  return book_a["author_lower"] > book_b["author_lower"]
+
+sorted_by_author_bubble = sorts.bubble_sort(books_small[:], compare_by_author_asc)
+sorted_by_author_quick = books_small[:]  # Copy to sort in-place
+sorts.quicksort(sorted_by_author_quick, 0, len(sorted_by_author_quick) - 1, compare_by_author_asc)
+
+# Sort by total length of author + title
+def compare_by_total_length(book_a, book_b):
+  sum_a = len(book_a["author"]) + len(book_a["title"])
+  sum_b = len(book_b["author"]) + len(book_b["title"])
+  return sum_a > sum_b
+
+# Load large dataset of books
+books_large = utils.load_books('books_large.csv')
+
+# Sort large list using both sorting algorithms
+sorted_by_length_bubble = sorts.bubble_sort(books_large[:], compare_by_total_length)
+sorted_by_length_quick = books_large[:]  # In-place sort
+sorts.quicksort(sorted_by_length_quick, 0, len(sorted_by_length_quick) - 1, compare_by_total_length)
+
+# Output results
+print("\nSorted by title (bubble sort):")
+for book in sorted_by_title:
+  print(book["author"])
+
+print("\nSorted by author (bubble sort):")
+for book in sorted_by_author_bubble:
+  print(book["author"])
+
+print("\nSorted by author (quicksort):")
+for book in sorted_by_author_quick:
+  print(book["author"])
+
+print("\nSorted by total length (bubble sort):")
+for book in sorted_by_length_bubble:
+  print(len(book["author"]) + len(book["title"]))
+
+print("\nSorted by total length (quicksort):")
+for book in sorted_by_length_quick:
+  print(len(book["author"]) + len(book["title"]))
--- sorts.py
+++ sorts.py
@@ -1,29 +1,71 @@
-import random
+import random  # Needed to randomly choose a pivot for quicksort
 
-def bubble_sort(arr):
+# ----------------------------
+# Bubble Sort Implementation
+# ----------------------------
+def bubble_sort(arr, comparison_function):
+  # Keep track of how many swaps occur (just for display)
   swaps = 0
+
+  # This flag will be used to control the while loop
   sorted = False
+
+  # Continue looping until the list is sorted (i.e., no swaps needed in a full pass)
   while not sorted:
-    sorted = True
+    sorted = True  # Assume it's sorted unless we find a pair that needs swapping
+
+    # Loop over each adjacent pair in the list
     for idx in range(len(arr) - 1):
-      if arr[idx] > arr[idx + 1]:
+      # Compare the current element and the next one
+      # If they are out of order based on the comparison_function, we swap them
+      if comparison_function(arr[idx], arr[idx + 1]):
+        # Since we found a swap, the list wasn't fully sorted
         sorted = False
+
+        # Swap the elements
         arr[idx], arr[idx + 1] = arr[idx + 1], arr[idx]
+
+        # Count the swap for stats
         swaps += 1
+
+  # Print total number of swaps performed
   print("Bubble sort: There were {0} swaps".format(swaps))
+
+  # Return the now-sorted array
   return arr
 
-def quicksort(list, start, end):
+# -----------------------------------
+# Quicksort Implementation (Recursive)
+# -----------------------------------
+def quicksort(list, start, end, comparison_function):
+  # Base case: if the list has one or zero elements in this slice, do nothing
   if start >= end:
     return
+
+  # Pick a random index between start and end to use as pivot
   pivot_idx = random.randrange(start, end + 1)
   pivot_element = list[pivot_idx]
+
+  # Move the pivot to the end for easier partitioning
   list[end], list[pivot_idx] = list[pivot_idx], list[end]
+
+  # This pointer keeps track of where the "less-than" region ends
   less_than_pointer = start
-  for i in range(start, end):
-    if pivot_element > list[i]:
+
+  # Partition step: move elements less than the pivot to the left
+  for i in range(start, end):  # Exclude the end index because that's now the pivot
+    # If current element should go before pivot (based on the comparison function),
+    # move it to the front of the list (within the "less than" region)
+    if comparison_function(pivot_element, list[i]):
+      # Swap current element with the one at less_than_pointer
       list[i], list[less_than_pointer] = list[less_than_pointer], list[i]
+
+      # Move the less_than_pointer forward (expanding the "less than" region)
       less_than_pointer += 1
+
+  # Finally, move the pivot element to its final sorted position
   list[end], list[less_than_pointer] = list[less_than_pointer], list[end]
-  quicksort(list, start, less_than_pointer - 1)
-  quicksort(list, less_than_pointer + 1, end)
+
+  # Recursively sort the two partitions (left and right of the pivot)
+  quicksort(list, start, less_than_pointer - 1, comparison_function)      # Left side
+  quicksort(list, less_than_pointer + 1, end, comparison_function)        # Right side
